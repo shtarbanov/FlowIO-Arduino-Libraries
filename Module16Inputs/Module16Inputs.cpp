39131af5bcac9f6dcebaf28618b64236f152dfd2
Yoav Luft <yoav.luft@prototyp.se>
2021-12-02 11:50:13 +0100

    Cleaned up sampling code; Expose sampling mode, settle time; add difference sampling;

--- Module16Inputs/Module16Inputs.cpp
+++ Module16Inputs/Module16Inputs.cpp
@@ -10,207 +10,148 @@ a dummy read followed by a short delay before each real read. The effect is even
 
 #include "Arduino.h"
 #include "Module16Inputs.h"
+#include <array>
 
 //Constructor
-Module16Inputs::Module16Inputs(){
-	enableModule();
-	pinMode(x, OUTPUT);
-	pinMode(s1, OUTPUT);
-	pinMode(s0, OUTPUT);
-	pinMode(_A4,INPUT);
-	pinMode(_A5,INPUT);
+Module16Inputs::Module16Inputs() : sampleIndex(0) {
+    enableModule();
+    reading.fill(0);
+    for (auto &buffer: samples) {
+        buffer.fill(0);
+    }
+    pinMode(x, OUTPUT);
+    pinMode(s1, OUTPUT);
+    pinMode(s0, OUTPUT);
+    pinMode(_A4, INPUT);
+    pinMode(_A5, INPUT);
 }
 
 //Methods
-void Module16Inputs::enableModule(){
-	digitalWrite(_enPin,OUTPUT);
-	pinMode(_enPin, HIGH);
-}
-void Module16Inputs::disableModule(){
-	pinMode(_enPin, LOW);
-}
-void Module16Inputs::setMuxesChannel(uint8_t n){
-	bool xVal = (n%2==1);
-	bool s1Val = !(n<9);
-	bool s0Val = (n==5 || n==6 || n==7 || n==8 || n==13 || n==14 || n==15 || n==16);
-	digitalWrite(x, xVal);
-	digitalWrite(s1, s1Val);
-	digitalWrite(s0, s0Val);
-	delay(settleTime); //required for the MCU signal to rise after switching.
-}
-uint16_t Module16Inputs::readChannel(uint8_t n){
-	if(n<1 || n>16) return -1;
-
-	uint8_t analogPin = (n==1 || n==2 || n==5 || n==6 || n==9 || n==10 || n==13 || n==14) ? A5 : A4;
-	setMuxesChannel(n); //this one has a 1ms delay built-in
-  analogRead(analogPin);
-  delay(settleTime);
-	return analogRead(analogPin);
-}
-uint16_t Module16Inputs::readChannelMovAvg(uint8_t n, uint8_t samples){
-	if(samples==1) return readChannel(n);
-
-	//we can either shift the array item by 1 before each new read,
-	//or we can just store each new value into a new index which would be far more efficient.
-	//To use the second approach, I will have the array index go up only to <samples and then back to 0.
-	//I must use an array size that will always be greater than samples. I will assume up to 10 samples.
-	sampleNumber++; //this is the array index
-	if(sampleNumber==samples) sampleNumber=0;
-	lastSamples[sampleNumber] = readChannel(n);
-	float avg=0;
-	for(uint8_t i=0; i<samples; i++){
-		avg += lastSamples[i];
-	}
-	return (uint16_t) avg/samples;
+void Module16Inputs::enableModule() {
+    digitalWrite(_enPin, OUTPUT);
+    pinMode(_enPin, HIGH);
 }
 
-void Module16Inputs::read16ChannelsMovAvgInto(uint16_t a[], uint8_t samples){ //argument is the array provided by user containing the averaged results only.
-	if(samples==1){
-		read16ChannelsInto(a);
-		return;
-	}
-	sampleNumber++; //this is the array index
-	if(sampleNumber==samples) sampleNumber=0;
-	read16ChannelsInto(lastSamplesAllChannels[sampleNumber]); //first index is the sample number (0 to 49). Second index is channel number. We don't write the second index in the argument.
-	//Now we want to average over the first index for all 16 channels.
-	float avgCh[16]={0};
-	for(uint8_t i=0; i<samples; i++){
-		//Keep these not in a loop for clarity.
-		avgCh[0] += lastSamplesAllChannels[i][0];
-		avgCh[1] += lastSamplesAllChannels[i][1];
-		avgCh[2] += lastSamplesAllChannels[i][2];
-		avgCh[3] += lastSamplesAllChannels[i][3];
-		avgCh[4] += lastSamplesAllChannels[i][4];
-		avgCh[5] += lastSamplesAllChannels[i][5];
-		avgCh[6] += lastSamplesAllChannels[i][6];
-		avgCh[7] += lastSamplesAllChannels[i][7];
-		avgCh[8] += lastSamplesAllChannels[i][8];
-		avgCh[9] += lastSamplesAllChannels[i][9];
-		avgCh[10] += lastSamplesAllChannels[i][10];
-		avgCh[11] += lastSamplesAllChannels[i][11];
-		avgCh[12] += lastSamplesAllChannels[i][12];
-		avgCh[13] += lastSamplesAllChannels[i][13];
-		avgCh[14] += lastSamplesAllChannels[i][14];
-		avgCh[15] += lastSamplesAllChannels[i][15];
-	}
-	for(uint8_t i=0; i<samples; i++){
-		a[i] = (uint16_t) avgCh[i]/samples;
-	}
+void Module16Inputs::disableModule() {
+    pinMode(_enPin, LOW);
 }
+
+void Module16Inputs::setDiffChannel(uint8_t channel) {
+    if (channel <= 16) {
+        diffChannel = channel;
+    }
+}
+
+uint32_t Module16Inputs::setSettleTime(uint32_t microseconds) {
+    uint32_t oldValue = settleTime;
+    settleTime = microseconds;
+    return oldValue;
+}
+
+void Module16Inputs::setSampleAveraging(SampleAveraging numSamples) {
+    sampleAveraging = numSamples;
+}
+
+void Module16Inputs::setMuxesChannel(uint8_t n) {
+    bool xVal = (n % 2 == 1);
+    bool s1Val = !(n < 9);
+    bool s0Val = (n == 5 || n == 6 || n == 7 || n == 8 || n == 13 || n == 14 || n == 15 || n == 16);
+    digitalWrite(x, xVal);
+    digitalWrite(s1, s1Val);
+    digitalWrite(s0, s0Val);
+    delayMicroseconds(settleTime); //required for the MCU signal to rise after switching.
+}
+
+uint16_t Module16Inputs::readChannel(uint8_t n) {
+    if (n < 1 || n > 16) return -1;
+
+    uint8_t analogPin = (n == 1 || n == 2 || n == 5 || n == 6 || n == 9 || n == 10 || n == 13 || n == 14) ? A5 : A4;
+    setMuxesChannel(n);
+    uint32_t sample = 0;
+    // SampleAveraging can be either 1, 2 or 4
+    for (int i = 0; i < (uint8_t)sampleAveraging; i++) {
+        sample += analogRead(analogPin);
+        delayMicroseconds(settleTime);
+    }
+    // If we have a difference channel set, sample that one also and diff the result.
+    if (diffChannel > 0 && diffChannel <= 16 && diffChannel != n) {
+        uint16_t diffSample = readChannel(diffChannel);
+        // Subtract the diff from the sample, or if diff is greater than the sample zero the sample to avoid overflow.
+        // TODO: Consider returning signed results to accommodate for diffing.
+        sample -= min(diffSample, sample);
+    }
+    return (uint16_t)((sample) / sampleAveraging);
+}
+
+// Why this exists?
+uint16_t Module16Inputs::readChannelMovAvg(uint8_t channel, uint8_t numSamples) {
+    samples[sampleIndex][channel] = readChannel(channel);
+    sampleIndex = (sampleIndex + 1) % BUFFER_SIZE;
+
+    uint16_t tmp = 0;
+    for (int i = 0; i < numSamples; i++) {
+        const buffer_t &sample = samples[(sampleIndex - numSamples - 1 + i) % BUFFER_SIZE];
+        // Sum a sample with the temporary buffer.
+        tmp += sample[channel];
+    }
+    tmp /= numSamples;
+    return tmp;
+}
+
+template<size_t N>
+void sum(const std::array <uint16_t, N> &a, std::array <uint16_t, N> &into) {
+    for (size_t i = 0; i < N; i++) {
+        into[i] += a[i];
+    }
+}
+
+template<size_t N>
+void divideArray(uint16_t by, std::array <uint16_t, N> &array) {
+    for (size_t i = 0; i < N; i++) {
+        array[i] /= by;
+    }
+}
+
+void Module16Inputs::read16ChannelsMovAvgInto(uint16_t out[16],
+                                              uint8_t numSamples) { //argument is the array provided by user containing the averaged results only.
+    // 1. Sample all inputs into the samples array.
+    // 2. Increment cyclic index i
+    // 3. Sum samples[((i - 1 - numSamples) .. (i - 1)) mod BUFFER_SIZE]
+    // 4. return average.
+
+    read16ChannelsInto(samples[sampleIndex]);
+    sampleIndex = (sampleIndex + 1) % BUFFER_SIZE;
+
+    std::array <uint16_t, INPUTS> tmp = {0};
+    for (int i = 0; i < numSamples; i++) {
+        const buffer_t &sample = samples[(sampleIndex - numSamples - 1 + i) % BUFFER_SIZE];
+        // Sum a sample with the temporary buffer
+        sum<INPUTS>(sample, tmp);
+    }
+    divideArray<INPUTS>(numSamples, tmp);
+    for (int i = 0; i < INPUTS; i++) {
+        out[i] = tmp[i];
+    }
+}
+
 //It is strange that I cannot pass an array
-void Module16Inputs::read16ChannelsInto(uint16_t a[]){ //The results are stored into the array data16Channels
-	//Read all 16 values and store them into the array passed in the argument. Index 0 is sensor 1.
-	setMuxesChannel(1);
-	analogRead(A5);
-  delay(settleTime);
-  analogRead(A5);
-  delay(settleTime);
-	a[0] = analogRead(A5);
+void Module16Inputs::read16ChannelsInto(uint16_t out[16]) { //The results are stored into the array data16Channels
+    //Read all 16 values and store them into the array passed in the argument. Index 0 is sensor 1.
+    for (int i = 0; i < 4; i++) {
+        out[i] = readChannel(i + 1);
+        out[i + 4] = readChannel(i + 5);
+        out[i + 8] = readChannel(i + 9);
+        out[i + 12] = readChannel(i + 13);
+    }
+}
 
-	setMuxesChannel(5);
-	analogRead(A5);
-	delay(settleTime);
-	analogRead(A5);
-	delay(settleTime);
-	a[4] = analogRead(A5);
-
-	setMuxesChannel(9);
-	analogRead(A5);
-  delay(settleTime);
-  analogRead(A5);
-  delay(settleTime);
-	a[8] = analogRead(A5);
-
-	setMuxesChannel(13);
-	analogRead(A5);
-  delay(settleTime);
-  analogRead(A5);
-  delay(settleTime);
-	a[12] = analogRead(A5);
-
-	setMuxesChannel(2);
-	analogRead(A5);
-	delay(settleTime);
-	analogRead(A5);
-	delay(settleTime);
-	a[1] = analogRead(A5);
-
-	setMuxesChannel(6);
-  analogRead(A5);
-  delay(settleTime);
-  analogRead(A5);
-  delay(settleTime);
-	a[5] = analogRead(A5);
-
-	setMuxesChannel(10);
-	analogRead(A5);
-  delay(settleTime);
-  analogRead(A5);
-  delay(settleTime);
-	a[9] = analogRead(A5);
-
-	setMuxesChannel(14);
-	analogRead(A5);
-  delay(settleTime);
-  analogRead(A5);
-  delay(settleTime);
-	a[13] = analogRead(A5);
-
-	setMuxesChannel(3);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[2] = analogRead(A4);
-
-	setMuxesChannel(7);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[6] = analogRead(A4);
-
-	setMuxesChannel(11);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[10] = analogRead(A4);
-
-	setMuxesChannel(15);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[14] = analogRead(A4);
-
-	setMuxesChannel(4);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[3] = analogRead(A4);
-
-	setMuxesChannel(8);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[7] = analogRead(A4);
-
-  setMuxesChannel(12);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[11] = analogRead(A4);
-
-	setMuxesChannel(16);
-	analogRead(A4);
-  delay(settleTime);
-  analogRead(A4);
-  delay(settleTime);
-	a[15] = analogRead(A4);
+void Module16Inputs::read16ChannelsInto(buffer_t &out) {
+    for (int i = 0; i < 4; i++) {
+        out[i] = readChannel(i + 1);
+        out[i + 4] = readChannel(i + 5);
+        out[i + 8] = readChannel(i + 9);
+        out[i + 12] = readChannel(i + 13);
+    }
 }
 /*
 ------------------------
--- Module16Inputs/Module16Inputs.h
+++ Module16Inputs/Module16Inputs.h
@@ -12,36 +12,84 @@ I can also have my FlowIO library inherit this library. See https://www.tutorial
 #define Module16Inputs_h
 
 #include "Arduino.h"
+#include <array>
 
-class Module16Inputs{
+using std::array;
+
+const size_t INPUTS = 16;
+const size_t BUFFER_SIZE = 10;
+
+typedef array <uint16_t, INPUTS> buffer_t;
+
+enum SampleAveraging {
+    SINGLE_SAMPLING = 1,
+    DOUBLE_SAMPLING = 2,
+    QUADRUPLE_SAMPLING = 4
+};
+
+class Module16Inputs {
 private:
-	uint8_t _enPin=2;
-	uint8_t x=26;
-	uint8_t s0=25;
-	uint8_t s1=24;
+    uint8_t _enPin = 2;
+    uint8_t x = 26;
+    uint8_t s0 = 25;
+    uint8_t s1 = 24;
 
-	uint8_t _A4=A4;
-	uint8_t _A5=A5;
-	uint8_t settleTime=1;
+    uint8_t _A4 = A4;
+    uint8_t _A5 = A5;
+    uint32_t settleTime = 1;
+    SampleAveraging sampleAveraging = DOUBLE_SAMPLING;
 
-	//The following arrays are for the moving average calculations.
-	//The arrays must be initialized, otherwise I get garbage initially.
-	//TODO: initialize the arrays with the first value read rather than 0 if possible.
-	uint16_t lastSamples[10] = {0}; //This array will hold the last samples (up to 50) for cases where only 1 channel is used.
-	uint16_t lastSamplesAllChannels[10][16] = {0}; //this will hold the last 50 samples for all 16 channels.
-	uint8_t sampleNumber=0; //index for the moving arverage array.
+    //The following arrays are for the moving average calculations.
+    //The arrays must be initialized, otherwise I get garbage initially.
+    //TODO: initialize the arrays with the first value read rather than 0 if possible.
+
+    buffer_t reading;
+    array <buffer_t, BUFFER_SIZE> samples;
+    size_t sampleIndex;
+
+    uint8_t diffChannel;
 
 public:
-	Module16Inputs(); //Constructor
-	void setMuxesChannel(uint8_t n);
-	void enableModule();
-	void disableModule();
-	uint16_t readChannel(uint8_t n); //rename to readInput
-	void read16ChannelsInto(uint16_t a[]); //argument is the array that will store the values.
+    Module16Inputs(); //Constructor
+    void setMuxesChannel(uint8_t n);
 
-	//Moving average functions
-	uint16_t readChannelMovAvg(uint8_t n, uint8_t samples);
-	void read16ChannelsMovAvgInto(uint16_t a[], uint8_t samples);
+    void enableModule();
+
+    void disableModule();
+
+    uint16_t readChannel(uint8_t n); //rename to readInput
+    void read16ChannelsInto(uint16_t out[16]); //argument is the array that will store the values.
+    void read16ChannelsInto(buffer_t &out); //argument is the array that will store the values.
+
+    //Moving average functions
+    uint16_t readChannelMovAvg(uint8_t n, uint8_t samples);
+
+    void read16ChannelsMovAvgInto(uint16_t out[16], uint8_t samples);
+
+    /**
+     * Select a channel whose readings will be subtracted from all other readings
+     * @param channel
+     */
+    void setDiffChannel(uint8_t channel);
+
+    /**
+     * Change the wait time between switching mux and sampling, in microseconds.
+     * Default is 1microsecond, which should be enough for current multiplexers that have 600ns max settle time.
+     * @param microseconds
+     * @return the previous value in microseconds
+     */
+    uint32_t setSettleTime(uint32_t microseconds);
+
+    /**
+     * Set the number consecutive samples to be taken.
+     * This is different from moving average sampling. In sample averaging, multiple samples are taken consecutively on
+     * the same input (without changing channels), with {settleTime} microseconds apart between sampling, and the result
+     * is averaged.
+     * In moving average sampling, samples (which may be averaged!) are saved in a buffer, to allow averaging over
+     * repeated calls.
+     * @param numSamples number of samples to take repeatedly and average. Default is 2, should be kept low.
+     */
+    void setSampleAveraging(SampleAveraging numSamples);
 };
 
 #endif
