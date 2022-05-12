e6a6dda0cc508974d0f6a04510b77a4c1c9a8ed2
shtarbanov <alims@media.mit.edu>
2022-05-12 14:51:48 -0400

    Organized folders

--- FlowIO_2022.1.24/Examples/Hardware_Test_Visual/Hardware_Test_Visual.ino
+++ FlowIO_2022.1.24/Examples/Hardware_Test_Visual/Hardware_Test_Visual.ino
@@ -1,252 +1,103 @@
+/*Press the button under the letter W to start a hardware test. When you first power up the system,
+ * the neoPixel will start dancing. When you start a hardware test, the pixel will stop dancing and not
+ * start dancing again until you return to mode 0, which happens after power cycling. If you are 
+ * in any testing mode, the behavior of the pixel indicates the status of the test.
+ * If there is a fixed light of some color - that indicates an error (color coded by error type).
+ * If the pixel blinks 3 times in green, this means the test you just ran was successful.
+ * If the pixel is off, it means it is waiting for you to run the next test.
+ * If the pixel is dancing, the hardware tests have not been started yet. 
+ * If the pixel blinks while changing color, that is the 20sec leak test.
+ */
+
 #include <Adafruit_TinyUSB.h> // for Serial
 #include <FlowIO.h>
-#define btnPin 7
-#define batteryPin A6
+#define btnPin 7 //TODO: Place this as literal into the FlowIO library
+#define batteryPin A6 //TODO: Place this as literal into the FlowIO library
 
 FlowIO flowio;
-int mode = 0;
-int prevMode = -1; //making this different so we enter the state machine on first start.
-bool dancingPixel = false;
-bool buttonState = 0;         // current state of the button
-bool prevButtonState = 0;     // previous state of the button
-float p0 = 0;
-float pinf=0;
-float pvac=0;
-
-unsigned long prevTime = millis();
+//#############---Hardware Test Vars---#################
+int testMode = 0;
+int prevTestMode = 0;
+bool dancingNeoPixel = true;
+bool btnReading = 0;   // current digital reading of the button
+bool btnState = 0;     // previous state of the button
+unsigned long btnToggleTime = 0;
+float p0 = 0; //starting pressure
+float pinf=0; //inflation pressure
+float pvac=0; //vacuum pressure
+unsigned long pixelChangeTime = millis();
+//#############---END: Hardware Test Vars---##############
 
 void setup(){
   flowio = FlowIO(GENERAL); //This must be done the very first item to minimize the click on startup (Bug #44 on Github)  
   //NOTE: You cannot do the object initialization together with the declaration, because it will cause the 
   //hardware to not work fine, even though it will compile fine.
   
-  pinMode(btnPin,INPUT_PULLUP);   
+  pinMode(btnPin,INPUT_PULLUP);   //This should go into the FlowIO library
   flowio.blueLED(HIGH);
 }
 
 void loop() {
-  if(dancingPixel==true){
-    if(millis() - prevTime > 300){
+  //#############---BEGIN: Hardware Test---#################
+  if(dancingNeoPixel==true){
+    if(millis() - pixelChangeTime > 300){
       flowio.pixel(random(0,6),random(0,6),random(0,6));
-      prevTime = millis();          
+      pixelChangeTime = millis();          
     }
   }
-  if(mode != prevMode){ //Execute the following only if the mode changes.
+  if(testMode != prevTestMode){ //Execute the following only if the testMode changes.
     //The code in each case below will execute ONLY ONCE, not continuously.
-    switch(mode){
-      case 0: //we come here when we first start the system
-        dancingPixel = true;
-        break;
+    switch(testMode){
       case 1: //SENSOR (Error = RED)
-        delay(500); //delay to allow enough time to remove finger from the button
-        sensorTest(); //this function does NOT change the dancingPixel variable, but stop the dance while running.
-        //Hence, the dance will automatically restat when we exit the sensorTest() and our main loop gets invoked again. 
+        dancingNeoPixel = false; //Note that this won't take effect until the NEXT iteration of the loop,
+        flowio.pixel(0,0,0);      //hence I must also manually stop the pixel.
+        sensorTest(); 
         break;
       case 2: //VALVES (Error = no sound)
-        delay(500); //delay to allow enough time to remove finger from the button
         valveClickTest();
         break;
       case 3: //INFLATION (Error = YELLOW)
-        delay(500); //delay to allow enough time to remove finger from the button
         inflationPumpTest();
         break;
       case 4: //VACUUM (Error = PURPLE)
-        delay(500); //delay to allow enough time to remove finger from the button
         vacuumPumpTest();
         break;
       case 5: //BATTERY (Error = PINK)
-        delay(500);
         batteryConnectionTest();
         break;
-      case 6:
-        delay(500); //LEAK
+      case 6: //PWM INFLATION (Error = sound)
+        pwmInflationTest();
+        break;
+      case 7: //PWM VACUUM (Error = sound)
+        pwmVacuumTest();
+        break;
+      case 8: //LEAK
         manualLeakTest();
         break;
-      case 7: //POWER_OFF
+      case 9: //POWER_OFF
+        flowio.pixel(11,11,11);
+        delay(100);
         flowio.powerOFF();
         break;
     }
-    prevMode = mode;
+    prevTestMode = testMode;
   }
-  buttonState = digitalRead(btnPin);
-  if(buttonState != prevButtonState){ //if buttonstate has changed.
-    if(buttonState == LOW)  //and if it is now pressed.
-      mode += 1;
-      if(mode>7) mode=0;
+  btnReading = digitalRead(btnPin);
+  if(btnReading != btnState){ //if btnReading has changed.
+    if(btnReading == LOW)  //and if it is now pressed.
+      testMode += 1;
+      if(testMode>9){ //this is only relevant in case the powerOFF() fails.
+        testMode=0;
+        dancingNeoPixel=true;
+      }
       delay(50); //debounce
   }
-  prevButtonState = buttonState; 
-
+  btnState = btnReading;  
+  //#############---END: Hardware Test---#################
 }
 
-//############################--Function Definitions--##################################
-//######################################################################################
-void sensorTest(){
-  flowio.pixel(0,0,0); //stop the pixel from dancing
-  //To get the correct pressure reading, we will open and close two ports first
-  flowio.openPorts(0b00000011);
-  delay(100);
-  flowio.stopAction(0xFF);
-  while(flowio.activateSensor()==false){ //FAILED
-    delay(10);
-    showFailure(1,0,0); //RED
-  }
-  p0=flowio.getPressure(PSI);
-  showSuccess(); //3 GREEN blinks
-}
-void valveClickTest(){
-  flowio.pixel(0,0,0); //stop the dancing pixel
-  delay(100);
-  flowio.openOutletValve();
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.closeOutletValve();
-  flowio.pixel(0,0,0);
-  delay(1000);
-  flowio.setPorts(0b00000001);
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.setPorts(0b00000000);
-  flowio.pixel(0,0,0);
-  delay(1000);
-  flowio.setPorts(0b00000010);
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.setPorts(0b00000000);
-  flowio.pixel(0,0,0);
-  delay(1000);
-  flowio.setPorts(0b00000100);
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.setPorts(0b00000000);
-  flowio.pixel(0,0,0);
-  delay(1000);
-  flowio.setPorts(0b00001000);
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.setPorts(0b00000000);
-  flowio.pixel(0,0,0);
-  delay(1000);
-  flowio.setPorts(0b00010000);
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.setPorts(0b00000000);
-  flowio.pixel(0,0,0);
-  delay(1000); 
-  flowio.openInletValve();
-  flowio.pixel(0,0,5);
-  delay(500);
-  flowio.closeInletValve();
-  flowio.pixel(0,0,0);
-  delay(1000);
-}
-void inflationPumpTest(){
-  flowio.pixel(0,0,0);
-  Serial.print("Inflation Pump Test in Progress....");
-  flowio.openInletValve();
-  flowio.startPump(1);
-  delay(100);
-  pinf=flowio.getPressure(PSI);
-  if(pinf<=(p0+0.4)){
-    showFailure(1,1,0); //YELLOW -> inflation pump failed
-  }
-  else{
-    dancingPixel = true; //this line is not needed, b/c this is the default state.
-  }
-  flowio.stopPump(1);
-  //release the pressure and close the valves:
-  flowio.openOutletValve();
-  delay(300);
-  flowio.closeOutletValve();
-  flowio.closeInletValve();
-}
-void vacuumPumpTest(){
-  flowio.pixel(0,0,0);
-  flowio.openOutletValve();
-  flowio.startPump(2);
-  delay(100);
-  pvac=flowio.getPressure(PSI);
-  if(pvac>=(p0-0.4)){
-    showFailure(1,0,1); //PURPLE -> vacuum pump failed
-  }
-  else{
-    dancingPixel = true; //this line is not needed, b/c this is the default state.
-  }
-  flowio.stopPump(2);
-  //release the pressure and close the valves
-  flowio.openInletValve();
-  delay(300);
-  flowio.closeOutletValve();
-  flowio.closeInletValve();
-}
-void batteryConnectionTest(){
-  flowio.pixel(0,0,0);
-  pinMode(batteryPin,INPUT);
-  int vbat0 = analogRead(batteryPin);
-  flowio.openInletValve();
-  flowio.openOutletValve();
-  delay(800);
-  int vbat1 = analogRead(batteryPin);
-  flowio.closeInletValve();
-  flowio.closeOutletValve();
-  if(vbat1<vbat0){
-    showSuccess();
-  }
-  else{
-    showFailure(5,1,1); //PINK
-  }
-}
-void manualLeakTest(){
-  flowio.pixel(0,0,0);
-  flowio.openInletValve();
-  //You have 60 seconds to check for leaks on the inlet valve.
-  for(int i=60; i>=0; i--){
-    flowio.pixel(0,0,0);
-    delay(100);
-    flowio.pixel(1,0,0);
-    delay(100);
-    flowio.pixel(0,0,0);
-    delay(100);
-    flowio.pixel(0,1,0);
-    delay(100);
-    flowio.pixel(0,0,0);
-    delay(100);
-    flowio.pixel(0,0,1);
-    delay(100);
-    flowio.pixel(0,0,0);
-    delay(100);
-    flowio.pixel(1,1,0);
-    delay(100);
-    flowio.pixel(0,0,0);
-    delay(100);
-    flowio.pixel(0,1,1);
-    delay(100);
-    
-  }
-  flowio.closeInletValve();
-}
-//############################--Function Definitions--##################################
-//######################################################################################
-void showSuccess(){
-  //show a confimation of test sucess by blinking GREEEN three times.
-  flowio.pixel(0,0,0);
-  delay(200);
-  flowio.pixel(0,5,0);
-  delay(200);
-  flowio.pixel(0,0,0);
-  delay(200);
-  flowio.pixel(0,5,0);
-  delay(200);
-  flowio.pixel(0,0,0);
-  delay(200);
-  flowio.pixel(0,5,0);
-  delay(200); 
-  flowio.pixel(0,0,0);
-  delay(200); 
-  dancingPixel=true; 
-}
-void showFailure(uint8_t r, uint8_t g, uint8_t b){
- //set the pixel to RED and don't allow dancing.
- flowio.pixel(r,g,b);
- dancingPixel=false; 
-}
+/* TODO: Integrate this code into the main firmware, and there is no need to force the user to remain in testing testMode to executing all tests.
+ * If I press the button, I should enter the hardware testing loop and the control loop should only cycle between the hardware testing code. Any extraneous code should
+ * not be part of the infinite loop. But maybe I shouldn't do that. If I want to do a hardware test, I may just want to run a one or two tests, and not having to be
+ * forced to run the entire sequence of tests.
+ */
